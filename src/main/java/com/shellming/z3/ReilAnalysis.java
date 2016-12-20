5a20d933e7e03d8ec234f3316aa421464fa3dff4
shellming <shellming1992@gmail.com>
2016-12-20 13:22:42 +0800

    update

---
+++ src/main/java/com/shellming/z3/ReilAnalysis.java
@@ -0,0 +1,22 @@
+package com.shellming.z3;
+
+import com.google.security.zynamics.binnavi.API.reil.ReilBlock;
+
+import java.util.List;
+
+/**
+ * Created by ruluo1992 on 12/18/2016.
+ */
+public class ReilAnalysis {
+
+
+    public ReilAnalysis() {
+    }
+
+    public void processFunction(RFunction function, boolean isEntry) {
+        List<ReilBlock> blocks = function.getBlocks();
+        for (ReilBlock block : blocks) {
+
+        }
+    }
+}
